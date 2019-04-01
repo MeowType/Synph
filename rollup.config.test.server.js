edaca28094e6ce4d6e697f0131c6c1b1149afea2
2A5F <Aditrc@outlook.com>
2019-04-01 22:43:25 +0800

    save

---
+++ rollup.config.test.server.js
@@ -0,0 +1,8 @@
+export default {
+    input: './test/tsout/test/test.server.js',
+    output: {
+        file: './test/server/test.server.js',
+        format: 'cjs',
+        name: 'test_server'
+    }
+};
