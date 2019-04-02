ac1aaa762801ee2fadbf86eceaa4c737c704a775
2A5F <Aditrc@outlook.com>
2019-04-02 16:44:19 +0800

    save

--- src/synph.tsx
+++ src/synph.tsx
@@ -7,5 +7,9 @@ export function Synph(props: {
     name: string,
     syntax: ISyntax[] | (() => IterableIterator<ISyntax>) | ((this: Make, ctx: Make) => void)
 }) {
-    return <div>{123}</div>
+    const { name, syntax } = props
+    return <div className='synph'>
+        <div>{name}</div>
+        <div></div>
+    </div>
 }
