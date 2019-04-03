711ad914b156590d5aa2942a502a92867871ba81
2A5F <Aditrc@outlook.com>
2019-04-04 01:36:03 +0800

    save

--- src/synph.tsx
+++ src/synph.tsx
@@ -1,13 +1,24 @@
 export * from './range'
 export * from './syntax'
 import React from 'react'
-import { ISyntax, BodyFunc } from './syntax';
+import { ISyntax, BodyFunc, Group, Maker } from './syntax';
 
 export function SynphItem(props: {
     name: string,
     syntax: ISyntax[] | BodyFunc
 }) {
     const { name, syntax } = props
+    const arr = syntax instanceof Array ? syntax : []
+    if (typeof syntax === 'function') {
+        if (syntax[Symbol.toStringTag] === 'GeneratorFunction') {
+            arr.push(new Group(name, [...(syntax as any)()])) 
+        } else {
+            const items = []
+            const ctx = Maker(items.push)
+            syntax.call(ctx, ctx)
+            arr.push(new Group(name, items))
+        }
+    }
     return <div className='synph'>
         <div>{name}</div>
         <div></div>
