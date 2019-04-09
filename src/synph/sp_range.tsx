f183457b07418f228fb626ed6147767dff64ac56
2A5F <Aditrc@outlook.com>
2019-04-09 17:52:03 +0800

    range

--- src/synph/sp_range.tsx
+++ src/synph/sp_range.tsx
@@ -7,11 +7,11 @@ export function SynphRange(props: { syn: Range } & div) {
     const { syn, className, ...p } = props
     return check_need_loop(syn,
         [<span key='-1'></span>, ...[syn.from, syn.to].flatMap((i, index) =>
-            [<section key={i.id} className='synph-option-item-box'>{SynphSyn(i)}</section>
+            [<section key={i.id} className='synph-range-item-box'>{SynphSyn(i)}</section>
                 , <span key={index}></span>])],
         items => <article className={className == null ? Class : `${className} ${Class}`} {...p}>
-        <section className='synph-option-box synph-option-items-box'>
-            <div className='synph-option-items'>
+        <section className='synph-range-box synph-range-items-box'>
+            <div className='synph-range-items'>
                 {...items}
             </div>
         </section>
